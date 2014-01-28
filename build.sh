2d4f1b8146b17f094be82d84e97deaac5fcd5c09
Yang Zhang <yaaang@gmail.com>
2014-01-28 13:42:18 -0800

    tweak

--- build.sh
+++ build.sh
@@ -1,13 +1,16 @@
 #!/bin/sh
+set -x
 
 # Add any build steps you need here
 
-uname -a
-lsb_release -a
-free -m
-top -b -n1
-ps auxf
+#uname -a
+#lsb_release -a
+#free -m
+#top -b -n1
+#ps auxf
 
 #sudo aptitude install libcmph-dev libcmph0
 unxz -k test/data/*.xz
+pwd
+ls
 g++ -static -std=c++0x -Wall -g3 -o level0 level0.cc -L. -lcmph
