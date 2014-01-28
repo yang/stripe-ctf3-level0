b33e407e566056d94bca0a004ce9ef0009ff7600
Yang Zhang <yaaang@gmail.com>
2014-01-28 13:39:26 -0800

    static

--- build.sh
+++ build.sh
@@ -10,4 +10,4 @@ ps auxf
 
 #sudo aptitude install libcmph-dev libcmph0
 unxz -k test/data/*.xz
-g++ -std=c++0x -Wall -g3 -o level0 level0.cc -L. -lcmph
+g++ -static -std=c++0x -Wall -g3 -o level0 level0.cc -L. -lcmph
