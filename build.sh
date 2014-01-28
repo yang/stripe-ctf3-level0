dabd58f0a5838f0c047a312329756801bfca0af3
Yang Zhang <yaaang@gmail.com>
2014-01-28 13:26:57 -0800

    CMPH

--- build.sh
+++ build.sh
@@ -1,3 +1,7 @@
 #!/bin/sh
 
 # Add any build steps you need here
+
+sudo aptitude install libcmph-dev libcmph0
+unxz -k test/data/*.xz
+g++ -std=c++0x -Wall -g3 -o level0 level0.cc -lcmph
---
+++ level0.cc
@@ -0,0 +1,104 @@
+#include <ctype.h>
+#include <cstdio>
+#include <string.h>
+#include <unordered_set>
+#include <cmph.h>
+#include <string>
+#include <sys/types.h>
+#include <sys/stat.h>
+#include <fcntl.h>
+#include <iostream>
+using namespace std;
+enum { DICTLEN = 2249138 };
+enum { INLEN = 2249138 };
+enum { MAPSIZE = 210680 };
+char dictbuf[DICTLEN];
+char inbuf[INLEN];
+typedef unsigned char uint8_t;
+typedef unsigned short uint16_t;
+typedef unsigned int uint32_t;
+uint32_t markers[MAPSIZE];
+struct strview { uint32_t data; };
+inline bool is_input(const strview& v) { return v.data & 0x80000000U; }
+inline uint8_t vlen(const strview& v) { return (v.data & 0x7f000000U) >> 24U; }
+inline uint32_t vstart(const strview& v) { return (v.data & 0x00ffffffU); }
+//inline unsigned int std::hash(const strview& v) {
+//  return XXH32(buf[start(v.data)], len(v.data), 0);
+//}
+int main(int argc, char **argv) {
+  int dictf = open("test/data/lower.txt", O_RDONLY);
+  //cout << dictf << endl;
+  size_t dictcount = read(dictf, dictbuf, DICTLEN);
+  //cout << dictcount << endl;
+  dictbuf[dictcount] = '\0';
+  close(dictf);
+  if (dictcount < DICTLEN) {
+    perror("read dict");
+    return 1;
+  }
+
+  int mapf = open("test/data/packed", O_RDONLY);
+  //cout << mapf << endl;
+  size_t mapcount = read(mapf, markers, MAPSIZE * sizeof(uint32_t));
+  //cout << mapcount << endl;
+  close(mapf);
+  if (mapcount < MAPSIZE * sizeof(uint32_t)) {
+    perror("read map");
+    return 1;
+  }
+  //for (int i = 0; i < 100; i++) {
+  //  cout << (int) markers[i] << ' ';
+  //}
+  //cout << endl;
+
+  size_t incount;
+  if (argc > 2)
+    incount = read(open(argv[2], O_RDONLY), inbuf, INLEN);
+  else
+    incount = read(STDIN_FILENO, inbuf, INLEN);
+  inbuf[incount] = '\0';
+
+  FILE *mphf = fopen("test/data/lower.txt.mph", "r");
+  cmph_t *hasher = cmph_load(mphf);
+  char *start = inbuf;
+  char lowered[99];
+  while (true) {
+    char *end = strpbrk(start, " \n");
+    bool stop = end == NULL;
+    if (end == NULL) end = inbuf + incount;
+    size_t len = end - start;
+    if (len == 0) break;
+    for (size_t i = 0; i < len; i++) {
+      lowered[i] = tolower(start[i]);
+    }
+    unsigned int hashed = cmph_search(hasher, lowered, (cmph_uint32) len);
+    strview v = { markers[hashed] };
+    //cout << "hash " << hashed << endl;
+    //cout << "query start " << (start - inbuf) << " len " << len << ": " << string(start, len) << endl;
+    //cout << "match start " << vstart(v) << " len " << (int) vlen(v) << ": " << string(dictbuf + vstart(v), vlen(v)) << endl;
+
+    if (string(lowered, len) != string(dictbuf + vstart(v), vlen(v))) {
+      cout << "<" << string(start, len) << ">";
+    } else {
+      cout << string(start, len);
+    }
+    if (stop) {
+      break;
+    }
+    cout << string(start + len, end + 1 - (start + len));
+    start = end + 1;
+  }
+  cmph_destroy(hasher);
+
+  return 0;
+}
+// import sys, threading
+// ds = 
+// def f(i):
+//   def g():
+//     ds[i] = set(open(sys.argv[1]).read().split('\n'))
+// ts = [threading.Thread(f).start() for i in xrange(4)]
+// for t in ts: t.join()
+// import re
+// contents = sys.stdin.read()
+// sys.stdout.write(re.sub(r'[^ \n]+', lambda m: '<'+m.group()+'>' if m.group().lower() not in d else m.group(), contents))
